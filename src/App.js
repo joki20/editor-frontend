c01c2e8cc39cbdf13108af5d952743769f3fa15f
Johan Kristoffersson <joki20@student.bth.se>
2021-09-14 21:59:44 +0100

    Modified axios to accept query parameters with POST in order to avoid problem with backslash characters

--- src/App.js
+++ src/App.js
@@ -3,22 +3,113 @@ import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import Toolbar from "./components/Toolbar";
 import ListAll from "./components/ListAll";
+import axios from 'axios'; // database requests
+
+// global variables
+let ck5editor;
+let selectIndex = 1; // start choice (create new)
+let chosen; // chosen <option> element, undefined if empty
+let id;
+let title;
+let text;
+
+function setEditor(editor) {
+    ck5editor = editor;
+    return ck5editor
+}
+
+function renderSelect(e) {
+    // get index of <select>
+    selectIndex = e.target.options.selectedIndex;
+
+    // get chosen <select> and extract id, title and text
+    chosen = document.getElementsByTagName("option")[selectIndex];
+    id = chosen.getAttribute("name");
+    title = chosen.innerText;
+    text = chosen.getAttribute("text")
+  
+    console.log("FÖRE TEXT")
+    console.log(text);
+    console.log("EFTER TEXT");
+
+    // Replace text content in ck5 to changed
+    ck5editor.setData(text);
+
+    // empty status div
+    document.getElementsByClassName("status")[0].innerHTML = "";
+}
+
+function createOrSave(e) {
+    e.preventDefault();
+
+    // get text with <p> tags
+    text = ck5editor.getData()
+
+    // if CREATE NEW was selected
+    if (selectIndex === 1) {
+        console.log("NEW DOCUMENT CREATED");
+        
+        // create title from text
+        title = text.substring(3, 15);
+        
+        // post as query /create?title=titel&content=text
+        axios.post(`https://jsramverk-editor-joki20.azurewebsites.net/create`, {
+            title: title,
+            content: text
+        })
+            .then(response => response.status)
+            .catch(err => console.warn(err));
+
+        console.log("SUCCESS")
+    
+        // set status text
+        document.getElementsByClassName("status")[0].innerHTML = "New document was created! Reload page";
+
+    } else { // else UPDATE DOCUMENT
+        console.log("DOCUMENT UPDATED")
+
+        // replace <option text> to current/changed content
+        chosen.setAttribute("text", text);
+
+        axios.post(`https://jsramverk-editor-joki20.azurewebsites.net/update/${id}/`, {
+            title: title,
+            content: text
+        })
+            .then(response => response.status)
+            .catch(err => console.warn(err));
+
+        // set status text
+        document.getElementsByClassName("status")[0].innerHTML = "Document was updated! Reload page!";
+
+    }
+}
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <Toolbar>
-                    <ListAll />
+                    <ListAll
+                        editorComponent={ck5editor}
+                        renderContentAfterChoice={renderSelect}
+                        createOrSaveDocument={createOrSave}
+                    />
                 </Toolbar>
                 <h2>Using CKEditor 5 build in React</h2>
                 <CKEditor
                     editor={ClassicEditor}
-                    data="<p>Hello from CKEditor 5!</p>"
+                    id="ckeditor"
+                    data="<p>Make a choice in the menu!</p>" 
                     onReady={(editor) => {
                         // You can store the "editor" and use when it is needed.
                         console.log("Editor is ready to use!", editor);
+                        setEditor(editor);
                     }}
+                    onChange={ ( event, editor ) => {
+                        const data = editor.getData();
+                        // console.log( { data } );
+                    } }
+
                 />
             </div>
         );
--- src/components/ListAll.js
+++ src/components/ListAll.js
@@ -1,69 +1,5 @@
 import React from 'react';
-import axios from 'axios';
-
-let chosen; // chosen element, undefined if no choice was made
-let id; // id of newly chosen select
-let title; // title of newly chosen select
-let text; // text of newly chosen select. Changes if save button is pressed
-let currentUpdateAddress; // update API address for chosen text
-let selectIndex = "not set"; // undefined if no choice is made, 1 if Create New is chosen
-
-// RENDER CHOSEN DOCUMENT INSIDE CKEDITOR
-const renderChoice = function (e) {
-    selectIndex = e.target.options.selectedIndex;
-
-    // set chosen
-    chosen = document.getElementsByTagName("option")[selectIndex];
-    id = chosen.getAttribute("name");
-    title = chosen.innerText;
-    text = chosen.getAttribute("text");
-
-    // empty status div
-    document.getElementsByClassName("status")[0].innerHTML = "";
-
-    // Replace text content in CKEDITOR
-    document.querySelector(".ck p").innerHTML = text;
-}
-
-// SAVE DOCUMENT IN MONGODB
-const createOrSaveDocument = (e) => {
-    // prevent reloading of page
-    e.preventDefault();
-
-    // set 'text' to current content
-    text = document.querySelector(".ck p").innerHTML;
-
-    // if CREATE NEW was selected (null meaning variable is undefined/not set), make post request to create new
-    if (selectIndex == "not set" || selectIndex === 1) {
-
-        console.log("NEW DOCUMENT CREATED");
-        // set title to part of text
-        title = text.slice(0, 10);
-
-        // encoded POST address
-        currentUpdateAddress = encodeURI(`https://jsramverk-editor-joki20.azurewebsites.net/create/${title}/${text}`);
-
-        // set content in mongoDB to current/changed content
-        axios.post(currentUpdateAddress);
-
-        document.getElementsByClassName("status")[0].innerHTML = "New document was created!";
-
-    } else { // else UPDATE DOCUMENT
-        console.log("DOCUMENT UPDATED")
-
-        // replace <option text> to current/changed content
-        chosen.setAttribute("text", text);
-
-        // set encoded POST address
-        currentUpdateAddress = encodeURI(`https://jsramverk-editor-joki20.azurewebsites.net/update/${id}/${title}/${text}`);
-
-        // set content in mongoDB to current/changed content
-        axios.post(currentUpdateAddress);
-
-        document.getElementsByClassName("status")[0].innerHTML = "Document was updated!";
-    }
-}
-
+import axios from 'axios'; // database requests
 
 class ListAll extends React.Component {
   state = {
@@ -79,21 +15,26 @@ class ListAll extends React.Component {
   }
 
   render() {
-      return (
-        <div>
+    return (
+      <div>
             <form className="documentChoices">
-                <select onChange={renderChoice}>
+                <select onChange={this.props.renderContentAfterChoice}>
                     <option defaultValue disabled={true}>
                         --- Create or edit document ---
                     </option>
-                    <option>Create new</option>
+                    <option text="">Create new</option>
                     <option disabled={true}></option>
 
                     {this.state.documents.map((doc) => (
-                        <option key={doc._id} name={doc._id} text={doc.content}>{doc.title}</option>
+                      <option
+                        key={doc._id}
+                        name={doc._id}
+                        title={doc.title}
+                        text={doc.content}>{doc.title}</option>
                     ))}
-                </select>
-                <input type="submit" onClick={createOrSaveDocument} className="btn save"></input>
+                  </select>
+                <input type="hidden"></input>
+                <input type="submit" onClick={this.props.createOrSaveDocument} className="btn save"></input>
             </form>
             <div className="status"></div>
         </div>
